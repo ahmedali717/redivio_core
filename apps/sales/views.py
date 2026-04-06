642d1038244d46f9200fac593a18905c0a596369
Ahmed Ali <ahmed.ali@nc.local>
2026-04-06 21:26:27 +0200

    Implement Sales Module 16

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.db import transaction
 
 # Models & Serializers
