e48ad853d9082aef99bd44ad99ba5ec18b8b0d87
Ahmed Ali <ahmed.ali@nc.local>
2026-04-10 19:49:32 +0200

    Update sales module with WMS integration and migrations 19 - Invoices

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.shortcuts import render, get_object_or_404
 from django.db import transaction
+from redivio_project.utils.pdf import render_to_pdf
 import decimal
 
 # Models & Serializers
